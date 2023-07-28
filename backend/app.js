25faa20fa7bc5f7317ae0f0945ece231d5a8b7a8
Jannis Krumbholz <jakru00@gmx.de>
2023-07-28 15:20:32 +0200

    sending some first json

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,16 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.json(object);
 });
 
 app.listen(3000, function () {
     console.log("Server is running on localhost3000");
 });
+
+var object = {
+    name: "Thomas",
+    age: 23
+}
+
+// https://stackoverflow.com/questions/19696240/proper-way-to-return-json-using-node-or-express
